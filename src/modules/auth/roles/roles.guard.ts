b6e17bc1df78edbeb4dd5cc30f7ce0f435af70bc
palash2397 <palashpatil55@gmail.com>
2026-02-18 19:07:28 +0530

    Add auth modules and utilities with JWT authentication and role-based access control

---
+++ src/modules/auth/roles/roles.guard.ts
@@ -0,0 +1,38 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from './roles.decorator';
+import { Role } from '../../../common/enums/role.enum';
+import { ApiResponse } from '../../../utils/helpers/ApiResponse';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true; // No role restriction
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!user || !requiredRoles.includes(user.role)) {
+      throw new ForbiddenException(
+        new ApiResponse(403, {}, 'Access is forbidden'),
+      );
+    }
+
+    return true;
+  }
+}
