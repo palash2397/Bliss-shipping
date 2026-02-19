511a4f1ec7848f1354edd6ae0530b136aa4066ee
palash2397 <palashpatil55@gmail.com>
2026-02-19 14:16:05 +0530

    Add authentication and role-based access control to merchant controller

--- src/modules/merchant/merchant.controller.ts
+++ src/modules/merchant/merchant.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { MerchantService } from './merchant.service';
+import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
+
+import { RoleGuard } from 'src/modules/auth/roles/roles.guard';
+import { Role } from 'src/common/enums/role.enum';
+import { Roles } from 'src/modules/auth/roles/roles.decorator';
+
+import { CreateMerchantDto } from './dto/create-profile.dto';
+
 
 @Controller('merchant')
 export class MerchantController {
   constructor(private readonly merchantService: MerchantService) {}
+ 
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Post('create')
+  @Roles(Role.ADMIN)
+  create(@Body() dto: CreateMerchantDto) {
+    return this.merchantService.createProfile(dto);
+  }
 }
