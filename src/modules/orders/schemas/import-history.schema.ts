b96b85ed9a42400d5cad6175fa7b69aafb405e0e
palash2397 <palashpatil55@gmail.com>
2026-03-13 15:27:22 +0530

    Update import history schema to use CSV_IMPORT_STATUS enum

--- src/modules/orders/schemas/import-history.schema.ts
+++ src/modules/orders/schemas/import-history.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
+import { CSV_IMPORT_STATUS } from 'src/common/enums/csv-import-history.enum';
 
 export type ImportHistoryDocument = ImportHistory & Document;
 
@@ -27,8 +28,8 @@ export class ImportHistory {
   failedRows: number;
 
   @Prop({
-    enum: ['SUCCESS', 'PARTIAL', 'FAILED'],
-    default: 'SUCCESS',
+    enum: CSV_IMPORT_STATUS,
+    default: CSV_IMPORT_STATUS.SUCCESS,
   })
   status: string;
 }
