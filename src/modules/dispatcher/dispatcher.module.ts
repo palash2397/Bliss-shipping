50877fe6db54979519416cd002b78e2bbb91fac7
palash2397 <palashpatil55@gmail.com>
2026-03-13 18:56:23 +0530

    Update dispatcher module with Mongoose imports for Order and User schemas

--- src/modules/dispatcher/dispatcher.module.ts
+++ src/modules/dispatcher/dispatcher.module.ts
@@ -2,7 +2,17 @@ import { Module } from '@nestjs/common';
 import { DispatcherService } from './dispatcher.service';
 import { DispatcherController } from './dispatcher.controller';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order, OrderSchema } from '../orders/schemas/order.schema';
+import { User, UserSchema } from '../user/schemas/user.schema';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Order.name, schema: OrderSchema },
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
   controllers: [DispatcherController],
   providers: [DispatcherService],
 })
