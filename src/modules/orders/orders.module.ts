900fb596221e8a54362053c83f31d73bfabc455f
palash2397 <palashpatil55@gmail.com>
2026-03-13 15:28:32 +0530

    Register ImportHistory schema in orders module

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -7,12 +7,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Order, OrderSchema } from './schemas/order.schema';
 import { User, UserSchema } from '../user/schemas/user.schema';
 import { Merchant, MerchantSchema } from '../merchant/schemas/merchant-profile.schema';
-
+import { ImportHistory, ImportHistorySchema } from './schemas/import-history.schema';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     MongooseModule.forFeature([{ name: Merchant.name, schema: MerchantSchema }]),
+    MongooseModule.forFeature([{ name: ImportHistory.name, schema: ImportHistorySchema }]),
   ],
   controllers: [OrdersController],
   providers: [OrdersService],
