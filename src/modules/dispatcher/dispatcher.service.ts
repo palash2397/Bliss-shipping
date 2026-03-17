f2eaefd69b2bd593259a67465a9d0f7c648d11b8
palash2397 <palashpatil55@gmail.com>
2026-03-17 18:16:46 +0530

    changes in dispatcher

--- src/modules/dispatcher/dispatcher.service.ts
+++ src/modules/dispatcher/dispatcher.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Order, OrderDocument } from '../orders/schemas/order.schema';
+import { AssignDriverDto } from './dto/assign-driver.dto';
+import { DELIVERY_STATUS } from '../../common/enums/delivery-status.enum';
+
+import { ApiResponse } from 'src/utils/helpers/ApiResponse';
+import { Msg } from 'src/utils/helpers/responseMsg';
+
 @Injectable()
-export class DispatcherService {}
+export class DispatcherService {
+    constructor(
+        @InjectModel(Order.name) private orderModel: Model<OrderDocument>,
+    ) {}
+
+    allOrders(){
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+    
+
+}
