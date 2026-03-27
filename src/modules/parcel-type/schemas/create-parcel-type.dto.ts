d5200d77e5d376c36dff05fc62b50c023f34ad99
palash2397 <palashpatil55@gmail.com>
2026-03-27 16:29:40 +0530

    Add ParcelType schema with mongoose properties

---
+++ src/modules/parcel-type/schemas/create-parcel-type.dto.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ParcelTypeDocument = ParcelType & Document;
+
+@Schema({ timestamps: true })
+export class ParcelType {
+  @Prop({ required: true })
+  name: string; // Small, Medium
+
+  @Prop({ required: true })
+  minWeight: number;
+
+  @Prop({ required: true })
+  maxWeight: number;
+
+  @Prop({ required: true })
+  priceMultiplier: number; // 1x, 1.5x, 2x
+
+  @Prop({ default: true })
+  isActive: boolean;
+}
+
+export const ParcelTypeSchema = SchemaFactory.createForClass(ParcelType);
