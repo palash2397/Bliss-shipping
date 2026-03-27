3ea22ad4a071df3bf681f69a3235dfd80481c55d
palash2397 <palashpatil55@gmail.com>
2026-03-27 16:16:24 +0530

    Add item category controller with create endpoint

--- src/modules/item-category/item-category.controller.ts
+++ src/modules/item-category/item-category.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Req,
+  UploadedFile,
+  Get,
+  Param,
+  Query,
+  Patch,
+} from '@nestjs/common';
 import { ItemCategoryService } from './item-category.service';
 
+import { RoleGuard } from 'src/modules/auth/roles/roles.guard';
+import { Role } from 'src/common/enums/role.enum';
+import { Roles } from 'src/modules/auth/roles/roles.decorator';
+import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
+
+
+@UseGuards(JwtAuthGuard, RoleGuard)
 @Controller('item-category')
 export class ItemCategoryController {
   constructor(private readonly itemCategoryService: ItemCategoryService) {}
+
+  @Post('create')
+  @Roles(Role.ADMIN)
+  create(@Body() dto: any) {
+    return this.itemCategoryService.create(dto);
+  }
 }
