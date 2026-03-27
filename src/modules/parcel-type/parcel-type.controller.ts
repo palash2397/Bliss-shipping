8404a64d10dd24f1078575429b6a3d972e3a4713
palash2397 <palashpatil55@gmail.com>
2026-03-27 16:42:23 +0530

    Add parcel-type controller with CRUD endpoints

--- src/modules/parcel-type/parcel-type.controller.ts
+++ src/modules/parcel-type/parcel-type.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Req,
+  UploadedFile,
+  Get,
+  Param,
+  Query,
+  Patch,
+} from '@nestjs/common';
 import { ParcelTypeService } from './parcel-type.service';
 
+import { RoleGuard } from 'src/modules/auth/roles/roles.guard';
+import { Role } from 'src/common/enums/role.enum';
+import { Roles } from 'src/modules/auth/roles/roles.decorator';
+import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
+
+import { CreateParcelTypeDto } from './dto/create-parcel-type.dto';
+
+@UseGuards(JwtAuthGuard, RoleGuard)
 @Controller('parcel-type')
 export class ParcelTypeController {
   constructor(private readonly parcelTypeService: ParcelTypeService) {}
+
+  @Post('create')
+  @Roles(Role.ADMIN, Role.USER, Role.DRIVER)
+  create(@Body() createParcelTypeDto: CreateParcelTypeDto) {
+    return this.parcelTypeService.create(createParcelTypeDto);
+  }
+
+  @Get('all')
+  @Roles(Role.ADMIN, Role.USER, Role.DRIVER)
+  findAll() {
+    return this.parcelTypeService.findAll();
+  }
+
+  @Get('/find/:id')
+  @Roles(Role.ADMIN, Role.USER, Role.DRIVER)
+  findOne(@Param('id') id: string) {
+    return this.parcelTypeService.findOne(id);
+  }
 }
