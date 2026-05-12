d1e4b3fd87f29c0753b9ac799cd2fa7dc804958e
palash2397 <palashpatil55@gmail.com>
2026-05-12 16:14:45 +0530

    Update static assets prefix to /bliss/uploads

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
 
   // Serve uploaded files statically
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
-    prefix: '/uploads',
+    prefix: '/bliss/uploads',
   });
 
   // enable global validation for DTOs
