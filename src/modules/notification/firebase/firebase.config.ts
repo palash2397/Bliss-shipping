3f9d35fe192a8743f2e59d39ce3434fa85704d01
palash2397 <palashpatil55@gmail.com>
2026-05-12 15:07:21 +0530

    Update GitHub workflow to use base64 encoded env file and remove npm cache

---
+++ src/modules/notification/firebase/firebase.config.ts
@@ -0,0 +1,20 @@
+import { Provider } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+
+export const FirebaseProvider: Provider = {
+  provide: 'FIREBASE_ADMIN',
+
+  useFactory: () => {
+    const firebaseConfig = JSON.parse(process.env.FIREBASE_JSON as string);
+
+    firebaseConfig.private_key = firebaseConfig.private_key.replace(/\\n/g, '\n');
+
+    if (!admin.apps.length) {
+      admin.initializeApp({
+        credential: admin.credential.cert(firebaseConfig),
+      });
+    }
+
+    return admin;
+  },
+};
